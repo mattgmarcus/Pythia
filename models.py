3513f4d31d9ce50818be253625e480d339d3de72
Matt Marcus <matt.g.marcus@gmail.com>
2015-02-11 23:21:34 -0500

    Most of the model fields will be setup properly except the ones with todos

--- models.py
+++ models.py
@@ -1,4 +1,5 @@
 from app import db
+from datetime import datetime
 
 class Loan(db.Model):
     __tablename__ = "loans"
@@ -38,7 +39,7 @@ class Loan(db.Model):
     revolving_balance = db.Column(db.Integer)
     revolving_util = db.Column(db.Float)
     total_accounts = db.Column(db.Integer)
-    initial_list_status = db.Column(db.Integer) # W = 0, F = 1
+    initial_list_status = db.Column(db.Integer) # w = 0, f = 1
     outstanding_principal = db.Column(db.Float)
     outstanding_principal_investors = db.Column(db.Float)
     total_payment = db.Column(db.Float)
@@ -57,61 +58,82 @@ class Loan(db.Model):
     policy_code = db.Column(db.Integer) # 1 = publicly available, 2 = not publicly available
 
     def __init__(self, data):
-        self.id = data["id"]
-        self.member_id = data["member_id"]
-        self.loan_amount = data["loan_amnt"]
-        self.funded_amount = data["funded_amnt"]
-        self.funded_amount_investors = data["funded_amnt_inv"]
-        self.term = data["term"]
-        self.interest_rate = data["int_rate"]
-        self.installment = data["installment"]
+        self.id = int(data["id"])
+        self.member_id = int(data["member_id"])
+        self.loan_amount = int(data["loan_amnt"])
+        self.funded_amount = int(data["funded_amnt"])
+        self.funded_amount_investors = int(data["funded_amnt_inv"])
+        self.term = int(data["term"].split(" ")[0])
+        self.interest_rate = float(data["int_rate"][0:-1])
+        self.installment = float(data["installment"])
         self.grade = data["grade"]
         self.sub_grade = data["sub_grade"]
         self.employee_title = data["emp_title"]
+        # TODO: parse month right
         self.employment_length = data["emp_length"]
+        # TODO: Get home ownership as one of a few values
         self.home_ownership = data["home_ownership"]
-        self.annual_income = data["annual_inc"]
-        self.is_income_verified = data["is_inc_v"]
-        self.issue_date = data["issue_d"]
+        self.annual_income = int(data["annual_inc"])
+        self.is_income_verified = (data["is_inc_v"] == "Verified")
+        self.issue_date = __convert_lc_date(data["issue_d"])
+        # TODO: Figure out all the possible statuses and map to numbers
         self.loan_status = data["loan_status"]
-        self.payment_plan = data["pymnt_plan"]
+        self.payment_plan = (data["pymnt_plan"] == "y")
         self.url = data["url"]
         self.description = data["desc"]
         self.purpose = data["purpose"]
         self.title = data["title"]
         self.zip_code = data["zip_code"]
         self.address_state = data["addr_state"]
-        self.debt_to_income = data["dti"]
-        self.delinq_2yrs = data["delinq_2yrs"]
-        self.earliest_credit_line = data["earliest_cr_line"]
-        self.inq_last_6mths = data["inq_last_6mths"]
-        self.mths_since_last_delinq = data["mths_since_last_delinq"]
-        self.mth_since_last_record = data["mths_since_last_record"]
-        self.open_credit_lines = data["open_acc"]
-        self.public_records = data["pub_rec"]
-        self.revolving_balance = data["revol_bal"]
-        self.revolving_util = data["revol_util"]
-        self.total_accounts = data["total_acc"]
-        self.initial_list_status = data["initial_list_status"]
-        self.outstanding_principal = data["out_prncp"]
-        self.outstanding_principal_investors = data["out_prncp_inv"]
-        self.total_payment = data["total_pymnt"]
-        self.total_payment_investors = data["total_pymnt_inv"]
-        self.total_received_principal = data["total_rec_prncp"]
-        self.total_received_interest = data["total_rec_int"]
-        self.total_received_late_fees = data["total_rec_late_fee"]
-        self.recoveries = data["recoveries"]
-        self.collection_recovery_fee = data["collection_recovery_fee"]
-        self.last_payment_date = data["last_pymnt_d"]
-        self.last_payment_amount = data["last_pymnt_amnt"]
-        self.next_payment_date = data["next_pymnt_d"]
-        self.last_credit_pulled_date = data["last_credit_pull_d"]
-        self.collections_12_mths = data["collections_12_mths_ex_med"]
-        self.mths_since_last_major_derog = data["mths_since_last_major_derog"]
-        self.policy_code = data["policy_code"]
+        self.debt_to_income = float(data["dti"])
+        self.delinq_2yrs = int(data["delinq_2yrs"])
+        self.earliest_credit_line = __convert_lc_date(data["earliest_cr_line"])
+        self.inq_last_6mths = int(data["inq_last_6mths"])
+
+        self.mths_since_last_delinq = __set_if_present(data["mths_since_last_delinq"])
+        self.mths_since_last_record = __set_if_present(data["mths_since_last_record"])
+
+        self.open_credit_lines = int(data["open_acc"])
+        self.public_records = int(data["pub_rec"])
+        self.revolving_balance = float(data["revol_bal"])
+        self.revolving_util = float(data["revol_util"][0:-1])
+        self.total_accounts = int(data["total_acc"])
+
+        if data["initial_list_status"] == "w":
+            self.initial_list_status = 0
+        else: # == "f"
+            self.initial_list_status = 1
+
+        self.outstanding_principal = float(data["out_prncp"])
+        self.outstanding_principal_investors = float(data["out_prncp_inv"])
+        self.total_payment = float(data["total_pymnt"])
+        self.total_payment_investors = float(data["total_pymnt_inv"])
+        self.total_received_principal = float(data["total_rec_prncp"])
+        self.total_received_interest = float(data["total_rec_int"])
+        self.total_received_late_fees = float(data["total_rec_late_fee"])
+        self.recoveries = float(data["recoveries"])
+        self.collection_recovery_fee = float(data["collection_recovery_fee"])
+        self.last_payment_date = __convert_lc_date(data["last_pymnt_d"])
+        self.last_payment_amount = float(data["last_pymnt_amnt"])
+        self.next_payment_date = __convert_lc_date(data["next_pymnt_d"])
+        self.last_credit_pulled_date = __convert_lc_date(data["last_credit_pull_d"])
+        self.collections_12_mths = int(data["collections_12_mths_ex_med"])
+
+        self.mths_since_last_major_derog = __set_if_present(data["mths_since_last_major_derog"])
+
+        self.policy_code = int(data["policy_code"])
 
         data.pop("id")
         self.json = { self.id: data }
+
+    def __convert_lc_date(self, date):
+        datetime.strptime(date, "%b-%Y")
+
+    def __set_if_present(self, value):
+        if not value:
+            return 0
+        else:
+            return int(value)
 
     def __repr__(self):
         return '<id {}>'.format(self.id)
