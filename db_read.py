aaede6707fa2c009f7e906da9424a9275c02c665
Yondon Fu <y.fu710@gmail.com>
2015-02-14 17:17:05 -0500

    added impetus to deal with NaN features

--- db_read.py
+++ db_read.py
@@ -28,25 +28,25 @@ def get_data():
 
   labels = map(lambda vector: 1 if vector[16] == 'Fully Paid' else 0, features)
 
-  def toordinal(vector):
-    d15 = vector[15].toordinal() if vector[15] != None else None
-    d26 = vector[26].toordinal() if vector[26] != None else None
-    d45 = vector[45].toordinal() if vector[45] != None else None
-    d45 = vector[47].toordinal() if vector[47] != None else None
-    d45 = vector[48].toordinal() if vector[48] != None else None
-    return d15, d26, d45
+  # def toordinal(vector):
+  #   d15 = vector[15].toordinal() if vector[15] != None else None
+  #   d26 = vector[26].toordinal() if vector[26] != None else None
+  #   d45 = vector[45].toordinal() if vector[45] != None else None
+  #   d45 = vector[47].toordinal() if vector[47] != None else None
+  #   d45 = vector[48].toordinal() if vector[48] != None else None
+  #   return d15, d26, d45
 
-  date_ordinals = map(toordinal, features)
-  date_ordinals = np.array(date_ordinals)
+  # date_ordinals = map(toordinal, features)
+  # date_ordinals = np.array(date_ordinals)
 
-  features = np.array(features)
-  features = sp.delete(features, 15, 1)
-  features = sp.delete(features, 15, 1)
-  features = sp.delete(features, 24, 1)
-  features = sp.delete(features, 42, 1)
-  features = sp.delete(features, 43, 1)
-  features = sp.delete(features, 43, 1)
-  features = np.concatenate((features, date_ordinals), axis=1)
+  # features = np.array(features)
+  # features = sp.delete(features, 15, 1)
+  # features = sp.delete(features, 15, 1)
+  # features = sp.delete(features, 24, 1)
+  # features = sp.delete(features, 42, 1)
+  # features = sp.delete(features, 43, 1)
+  # features = sp.delete(features, 43, 1)
+  # features = np.concatenate((features, date_ordinals), axis=1)
 
   return features, labels
 
--- pythia.py
+++ pythia.py
@@ -5,17 +5,22 @@ from db_read import get_data
 from sklearn.ensemble import RandomForestClassifier
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.cross_validation import train_test_split
-from sklearn.preprocessing import OneHotEncoder
+from sklearn.preprocessing import Imputer
 
 if __name__=="__main__":
   features, labels = get_data()
-  features = [dict(r.iteritems()) for r in features]
-  vect = DictVectorizer()
-  vectorized_sparse = vect.fit_transform(features)
-  features = vectorized_sparse.toarray()
-  # features = [dict(enumerate(features)) for feature in features]
-  # vect = DictVectorizer(sparse=False)
-  # features = vect.fit_transform(features)
+  # features = [dict(r.iteritems()) for r in features]
+  # vect = DictVectorizer()
+  # vectorized_sparse = vect.fit_transform(features)
+  # features = vectorized_sparse.toarray()
+  
+  features = [dict(enumerate(feature)) for feature in features]
+  vect = DictVectorizer(sparse=False)
+  features = vect.fit_transform(features)
+
+  imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
+  imp.fit(features)
+  features = imp.transform(features)
 
   train_features, test_features, train_labels, test_labels =\
   train_test_split(features, labels, test_size=.3)
@@ -26,11 +31,11 @@ if __name__=="__main__":
   test_features: size (k, n) matrix of k samples of n features
   train_labels: size (k, 1) vector of the k training samples
   """
-  classifier = RandomForestClassifier(n_estimators=10, \
+  classifier = RandomForestClassifier(n_estimators=50, \
                                       n_jobs=-1, \
                                       verbose=1)
   classifier.fit(train_features, train_labels)
   print "Random forest predicted with ", \
-        "{0:.0f}%".format(classifier(test_features, test_labels)), \
+        "{0:.0f}%".format(classifier.score(test_features, test_labels)), \
         "accuracy."
   # print features, labels
