06f1a54e04e518147df7c13d59526bf04efa6176
Shuo Zheng <shuo.zheng@outlook.com>
2015-02-13 22:39:20 -0500

    trying to feed to random forest

--- db_read.py
+++ db_read.py
@@ -4,33 +4,22 @@ import numpy as np
 import scipy as sp
 from models import Loan
 
-def get_data(table):
-	# return Loan.query.filter(Loan.loan_status == 'Current').all()
-	sql = text("select * from loans where loan_status != \'Current\'")
-	result = c.execute(sql)
+def get_data():
+  meta = MetaData()
 
-	test = []
+  engine = create_engine("postgresql://localhost/pythia_dev", isolation_level="READ UNCOMMITTED")
+  sql = text("select * from loans where loan_status != \'Current\'")
+  result = engine.connect().execute(sql)
+  loans = Table('loans', meta, autoload=True, autoload_with=engine)
 
-	for row in result:
-		test.append(row)
+  features = []
 
-	return test
+  for row in result:
+    features.append(row)
 
-engine = create_engine("postgresql://localhost/pythia_dev",
-	isolation_level="READ UNCOMMITTED")
+  labels = map(lambda vector: 1 if vector[16] == 'Fully Paid' else 0, features)
 
-c = engine.connect()
+  features = np.array(features)
+  features = sp.delete(features, 16, 1)
 
-meta = MetaData()
-
-loans = Table('loans', meta, autoload=True, autoload_with=engine)
-
-test_data = get_data(loans)
-labels = map(lambda vector: 1 if vector[16] == 'Fully Paid' else 0, test_data)
-
-test_array = np.array(test_data)
-test_array = sp.delete(test_array, 16, 1)
-
-# print test_array
-# print test_array.shape
-# print labels
+  return features, labels
--- pythia.py
+++ pythia.py
@@ -1,19 +1,22 @@
 #!/usr/bin/env python
 import numpy as np
 import scipy as sp
+from db_read import get_data
 from sklearn.ensemble import RandomForestClassifier
 
 if __name__=="__main__":
+  features, labels = get_data()
   """
   train_features: size (m, n) matrix of m samples of n features
   train_labels: size (m, 1) vector of the m training samples
   test_features: size (k, n) matrix of k samples of n features
   train_labels: size (k, 1) vector of the k training samples
   """
-  classifier = RandomForestClassifier(n_estimators=100, \
-                                      n_jobs=-1, \
-                                      verbose=1)
-  classifier.fit(train_features, train_labels)
-  print "Random forest predicted with ", \
-        "{0:.0f}%".format(classifier(test_features, test_labels)), \
-        "accuracy."
+  # classifier = RandomForestClassifier(n_estimators=100, \
+  #                                     n_jobs=-1, \
+  #                                     verbose=1)
+  # classifier.fit(train_features, train_labels)
+  # print "Random forest predicted with ", \
+  #       "{0:.0f}%".format(classifier(test_features, test_labels)), \
+  #       "accuracy."
+  print features, labels
