92e40ab12b8d27c3fd9c5a795fb5e77cbd1e10a9
Matt Marcus <matt.g.marcus@gmail.com>
2015-02-14 14:36:50 -0500

    Forget to shuffle data before splitting

--- partition_data.py
+++ partition_data.py
@@ -3,6 +3,7 @@ from sqlalchemy.sql import *
 from sqlalchemy import *
 from models import Loan
 import os
+from random import shuffle
 
 if __name__=="__main__":
   engine = create_engine(os.environ['DATABASE_URL'], isolation_level="READ UNCOMMITTED")
@@ -12,6 +13,8 @@ if __name__=="__main__":
   loan_ids = []
   for loan in all_loans:
       loan_ids.append(loan.id)
+
+  shuffle(loan_ids)
 
   train_ids = tuple(loan_ids[0:124037])
   test_ids = tuple(loan_ids[124037:177196])
