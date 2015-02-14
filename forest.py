e07c5e3f85e3d3f973210ace3e6b4dd62fc6c000
Yondon Fu <y.fu710@gmail.com>
2015-02-14 17:45:23 -0500

    some skeleton func for forest

--- forest.py
+++ forest.py
@@ -1,1 +1,19 @@
 from tree import DecisionTreeClassifier
+
+class RandomForestClassifier(self):
+	def __init__(self):
+		self.base_estimator = None
+
+	def fit(self, X, Y):
+		pass
+
+	def get_oob_score(self, X, Y):
+		pass
+
+	def predict(self, X):
+		pass
+
+	def feature_relevances(self):
+		pass
+
+
