07abd4158498654e69834354f08303fec6a7776e
Yondon Fu <y.fu710@gmail.com>
2015-02-14 20:46:01 -0500

    Basic working. Need clean up

--- db_read.py
+++ db_read.py
@@ -4,46 +4,101 @@ import numpy as np
 import scipy as sp
 from models import Loan
 import random
+from random import shuffle
+
+def sort_labels(vector):
+  r = dict(vector.items())
+  if r["loan_status"] == "Fully Paid":
+    return 1
+  else:
+    return 0
 
 def get_data():
-  fields = "loan_amount, funded_amount, funded_amount_investors, term, interest_rate, installment, grade, sub_grade, employee_title, employment_length, home_ownership, annual_income, is_income_verified, loan_status, payment_plan, purpose, zip_code, address_state, debt_to_income, delinq_2yrs, inq_last_6mths, mths_since_last_delinq, open_credit_lines, public_records, revolving_balance, revolving_util, total_accounts, initial_list_status, outstanding_principal, outstanding_principal_investors, total_payment, total_payment_investors, total_received_principal, total_received_interest, total_received_late_fees, recoveries, collection_recovery_fee, last_payment_amount, collections_12_mths, mths_since_last_major_derog, policy_code, mths_since_last_record"
+  fields_with_loan_status = "loan_amount, funded_amount, funded_amount_investors, term, interest_rate, installment, grade, sub_grade, employee_title, employment_length, home_ownership, annual_income, is_income_verified, loan_status, payment_plan, purpose, zip_code, address_state, debt_to_income, delinq_2yrs, inq_last_6mths, mths_since_last_delinq, open_credit_lines, public_records, revolving_balance, revolving_util, total_accounts, initial_list_status, outstanding_principal, outstanding_principal_investors, total_payment, total_payment_investors, total_received_principal, total_received_interest, total_received_late_fees, recoveries, collection_recovery_fee, last_payment_amount, collections_12_mths, mths_since_last_major_derog, policy_code, mths_since_last_record"
+  fields = "loan_amount, funded_amount, funded_amount_investors, term, interest_rate, installment, grade, sub_grade, employee_title, employment_length, home_ownership, annual_income, is_income_verified, payment_plan, purpose, zip_code, address_state, debt_to_income, delinq_2yrs, inq_last_6mths, mths_since_last_delinq, open_credit_lines, public_records, revolving_balance, revolving_util, total_accounts, initial_list_status, outstanding_principal, outstanding_principal_investors, total_payment, total_payment_investors, total_received_principal, total_received_interest, total_received_late_fees, recoveries, collection_recovery_fee, last_payment_amount, collections_12_mths, mths_since_last_major_derog, policy_code, mths_since_last_record"
+  # fields = "grade, sub_grade"
+  # fields_with_loan_status = "grade, sub_grade, loan_status"
+  # fields = 'loan_status'
+  # fields_with_loan_status = 'loan_status'
 
+  # fields = "purpose"
+  # fields_with_loan_status = "purpose, loan_status"
 
   meta = MetaData()
 
   engine = create_engine("postgresql://localhost/pythia_dev", isolation_level="READ UNCOMMITTED")
-  sql = text("select %s from loans where loan_status != \'Current\'" % fields)
-  result = engine.connect().execute(sql)
+  # sql_with_loan_status = text("select %s from loans where loan_status in ('Charged Off', 'Fully Paid')" % fields_with_loan_status)
+  sql_for_charged_off_with_loan_status = text("select %s from loans where loan_status in ('Charged Off')" % fields_with_loan_status)
+  sql_for_paid_off_with_loan_status = text("select %s from loans where loan_status in ('Fully Paid')" % fields_with_loan_status)
+  sql_for_charged_off = text("select %s from loans where loan_status in ('Charged Off')" % fields)
+  sql_for_paid_off = text("select %s from loans where loan_status in ('Fully Paid')" % fields)
+  # sql = text("select %s from loans where loan_status in ('Charged Off', 'Fully Paid')" % fields)
+  # result_with_loan_status = engine.connect().execute(sql_with_loan_status)
+  # result = engine.connect().execute(sql)
+  result_charged_with_loan_status = engine.connect().execute(sql_for_charged_off_with_loan_status)
+  result_paid_with_loan_status = engine.connect().execute(sql_for_paid_off_with_loan_status)
+  result_charged = engine.connect().execute(sql_for_charged_off)
+  result_paid = engine.connect().execute(sql_for_paid_off)
   loans = Table('loans', meta, autoload=True, autoload_with=engine)
 
   features = []
+  features_with_loan_status = []
 
-  for row in result:
+  half_sample_size = 500
+
+  count = 0
+  for row in result_charged:
+    print row[0]
+    i
+    if count == half_sample_size:
+      break
     features.append(row)
+    count += 1
 
-  features = random.sample(features, 1000)
+  count = 0
+  for row in result_paid:
+    if count == half_sample_size:
+      break
+    features.append(row)
+    count += 1
 
-  labels = map(lambda vector: 1 if vector[16] == 'Fully Paid' else 0, features)
+  count = 0
+  for row in result_charged_with_loan_status:
+    if count == half_sample_size:
+      break
+    features_with_loan_status.append(row)
+    count += 1
 
-  # def toordinal(vector):
-  #   d15 = vector[15].toordinal() if vector[15] != None else None
-  #   d26 = vector[26].toordinal() if vector[26] != None else None
-  #   d45 = vector[45].toordinal() if vector[45] != None else None
-  #   d45 = vector[47].toordinal() if vector[47] != None else None
-  #   d45 = vector[48].toordinal() if vector[48] != None else None
-  #   return d15, d26, d45
+  count = 0
+  for row in result_paid_with_loan_status:
+    if count == half_sample_size:
+      break
+    features_with_loan_status.append(row)
+    count += 1
 
-  # date_ordinals = map(toordinal, features)
-  # date_ordinals = np.array(date_ordinals)
 
-  # features = np.array(features)
-  # features = sp.delete(features, 15, 1)
-  # features = sp.delete(features, 15, 1)
-  # features = sp.delete(features, 24, 1)
-  # features = sp.delete(features, 42, 1)
-  # features = sp.delete(features, 43, 1)
-  # features = sp.delete(features, 43, 1)
-  # features = np.concatenate((features, date_ordinals), axis=1)
+  # for row in result:
+  #   features.append(row)
 
+  # for row2 in result_with_loan_status:
+  #   features_with_loan_status.append(row2)
+
+  #features = random.sample(features, 10000)
+  # features = features[0:10000]
+  # features_with_loan_status = features_with_loan_status[0:10000]
+
+  labels = map(sort_labels, features_with_loan_status)
+
+
+  f1 = []
+  f2 = []
+  index_shuf = range(len(features))
+  shuffle(index_shuf)
+  for i in index_shuf:
+    f1.append(features[i])
+    f2.append(labels[i])
+
+  features = f1
+  labels = f2
   return features, labels
 
--- pythia.py
+++ pythia.py
@@ -9,6 +9,7 @@ from sklearn.preprocessing import Imputer
 
 if __name__=="__main__":
   features, labels = get_data()
+  print features, labels
   # features = [dict(r.iteritems()) for r in features]
   # vect = DictVectorizer()
   # vectorized_sparse = vect.fit_transform(features)
@@ -31,11 +32,14 @@ if __name__=="__main__":
   test_features: size (k, n) matrix of k samples of n features
   train_labels: size (k, 1) vector of the k training samples
   """
-  classifier = RandomForestClassifier(n_estimators=50, \
+  classifier = RandomForestClassifier(n_estimators=1000, \
                                       n_jobs=-1, \
-                                      verbose=1)
-  classifier.fit(train_features, train_labels)
+                                      verbose=1,
+                                      oob_score=True,
+                                      max_features=None)
+  classifier.fit(train_features, train_labels).oob_score_
   print "Random forest predicted with ", \
         "{0:.0f}%".format(classifier.score(test_features, test_labels)), \
         "accuracy."
+  print classifier.score(test_features, test_labels)
   # print features, labels
