7df705014ba426b04b650fa202543dc6309439c0
Matt Marcus <matt.g.marcus@gmail.com>
2015-03-07 13:01:18 -0500

    Setup basic layout for regressor. Using sklearn now

--- pythia.py
+++ pythia.py
@@ -4,6 +4,7 @@ import numpy as np
 import scipy as sp
 from db_read import *
 #from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import RandomForestRegressor
 from sklearn.feature_extraction import DictVectorizer
 from sklearn.cross_validation import train_test_split
 from sklearn.preprocessing import Imputer
@@ -20,7 +21,7 @@ def accept(args):
       "employment_length",
       # "policy_code"
   ]
-  feature_string = ','.join(feature_fields)
+  feature_string = ",".join(feature_fields)
 
   label_fields = []
 
@@ -61,20 +62,133 @@ def accept(args):
   # importances = classifier.feature_importances_
   # print zip(vect.get_feature_names(), importances)
 
-def loan_status_labels(label):
-  if label[0] == "Fully Paid":
+def loan_status_labels(features):
+  if features[0] == "Fully Paid":
     return 1
   else:
     return 0
 
+def loan_grade_labels(features):
+  grade_dict = {"A": 0, "B": 1, "C": 2,
+                "D": 3, "E": 4, "F": 5, "G": 6}
+
+  label = features[0]
+  # Examples: A1 = 1, G5 = 35, C2=12
+  return grade_dict[label[0]]*5 + int(label[1])
+
+def remove_label(features, last_label_index):
+  return features[last_label_index+1:]
+
 def remove_loan_status(features):
-  return features[:1]
+  return remove_label(features, 0)
+
+def remove_loan_grade(features):
+  return remove_label(features, 0)
 
 def grade(args):
   feature_fields = [
+    #"loan_status",
+    "loan_amount",
+    "funded_amount",
+    "funded_amount_investors",
+    "term",
+    # "interest_rate",
+    "installment",
+    #"grade",
+    #"sub_grade",
+    #"employee_title",
+    "employment_length",
+    "home_ownership",
+    "annual_income",
+    #"is_income_verified",
+    #"payment_plan",
+    #"purpose",
+    #"zip_code",
+    #"address_state",
+    "debt_to_income",
+    "delinq_2yrs",
+    "inq_last_6mths",
+    "mths_since_last_delinq",
+    "open_credit_lines",
+    "public_records",
+    "revolving_balance",
+    "revolving_util",
+    "total_accounts",
+    #"initial_list_status",
+    #"outstanding_principal",
+    #"outstanding_principal_investors",
+    #"total_payment",
+    #"total_payment_investors",
+    #"total_received_principal",
+    #"total_received_interest",
+    #"total_received_late_fees",
+    #"recoveries",
+    #"collection_recovery_fee",
+    #"last_payment_amount",
+    "collections_12_mths",
+    "mths_since_last_major_derog",
+    #"policy_code",
+    "mths_since_last_record"
   ]
   label_fields = [
+    # "grade",
+    "sub_grade",
   ]
+
+  feature_string = ",".join(label_fields + feature_fields)
+  sql = text("select %s from loans" % feature_string)
+  features, labels = get_data("loans",
+                              sql=sql,
+                              num_samples=args.numsamples,
+                              label_mapping=loan_grade_labels,
+                              features_processing=remove_loan_grade,
+                              shuffle=True,
+                              testing=True)
+
+  features = [dict(enumerate(feature)) for feature in features]
+  vect = DictVectorizer(sparse=False)
+  #print features
+  features = vect.fit_transform(features)
+  #print features.shape
+
+  # imp = Imputer(missing_values='NaN', strategy='mean', axis=0)
+  # imp.fit(features)
+  # features = imp.transform(features)
+  # print features.shape
+
+  train_features, test_features, train_labels, test_labels =\
+  train_test_split(features, labels, test_size=.3)
+
+  """
+  train_features: size (m, n) matrix of m samples of n features
+  train_labels: size (m, 1) vector of the m training samples
+  test_features: size (k, n) matrix of k samples of n features
+  train_labels: size (k, 1) vector of the k training samples
+  """
+  classifier = RandomForestRegressor(n_estimators=args.numtrees, \
+                                      n_jobs=-1, \
+                                      verbose=3,
+                                      oob_score=True,
+                                      max_features=None)
+
+  # classifier = DecisionTreeClassifier(10000)
+  # classifier = sklearn.tree.DecisionTreeClassifier()
+
+  # classifier = RandomForestRegressor(n_trees=args.numtrees,
+  #   n_jobs=8,
+  #   max_depth=10000)
+
+  classifier.fit(train_features, train_labels)
+  importances = classifier.feature_importances_
+  print zip(vect.get_feature_names(), importances)
+  # print vect.feature_names_
+  # print vect.vocabulary_
+  # print vect.inverse_transform(importances)
+  # print importances
+  # print importances.shape
+
+  print classifier.score(test_features, test_labels)
+
 
 def quality(args):
   feature_fields = [
@@ -124,7 +238,7 @@ def quality(args):
   label_fields = [
       "loan_status"
   ]
-  feature_string = ','.join(label_fields) + ',' + ','.join(feature_fields)
+  feature_string = ",".join(label_fields + feature_fields)
   sql = text("select %s from loans" % feature_string)
   features, labels = get_data("loans",
                               sql=sql,
@@ -160,7 +274,7 @@ def quality(args):
   test_features: size (k, n) matrix of k samples of n features
   train_labels: size (k, 1) vector of the k training samples
   """
-  # classifier = RandomForestClassifier(n_estimators=1000, \
+  # classifier = RandomForestClassifier(n_estimators=args.numtrees, \
   #                                     n_jobs=-1, \
   #                                     verbose=3,
   #                                     oob_score=True,
@@ -201,6 +315,8 @@ if __name__=="__main__":
     accept(args)
   elif args.test == "quality":
     quality(args)
+  elif args.test == "grade":
+    grade(args)
   else:
     parser.print_help()
 
