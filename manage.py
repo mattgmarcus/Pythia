0410e47f41cfa1202824ceda37d9be07664db1fc
Matt Marcus <matt.g.marcus@gmail.com>
2015-03-15 19:04:56 -0400

    Got init of db working

--- manage.py
+++ manage.py
@@ -3,12 +3,12 @@ from flask.ext.migrate import Migrate, MigrateCommand
 import os
 
 from app import app, db
-# app.config.from_object(os.environ['APP_SETTINGS'])
+app.config.from_object("config.DevelopmentConfig")
 
-# migrate = Migrate(app, db)
-# manager = Manager(app)
+migrate = Migrate(app, db)
+manager = Manager(app)
 
-# manager.add_command('db', MigrateCommand)
+manager.add_command('db', MigrateCommand)
 
-# if __name__ == '__main__':
-#     manager.run()
+if __name__ == '__main__':
+    manager.run()
--- manage_test.py
+++ manage_test.py
@@ -6,16 +6,16 @@ import os
 
 
 app = Flask(__name__)
-# app.config.from_object("config.TestingConfig")
+app.config.from_object("config.TestingConfig")
 db = SQLAlchemy(app)
 
 
 # app.config.from_object(os.environ['APP_SETTINGS'])
 
-# migrate = Migrate(app, db)
-# manager = Manager(app)
+migrate = Migrate(app, db)
+manager = Manager(app)
 
-# manager.add_command('db', MigrateCommand)
+manager.add_command('db', MigrateCommand)
 
-# if __name__ == '__main__':
-#     manager.run()
+if __name__ == '__main__':
+    manager.run()
